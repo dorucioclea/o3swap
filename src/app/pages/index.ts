e930d39e777691f6fdaed33161b21f78241f4406
bao <965001935@qq.com>
2021-04-01 20:41:15 +0800

    feat: add dashboard

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -2,3 +2,4 @@ export { HomeComponent } from './home/home.component';
 export { SwapComponent } from './swap/swap.component';
 export { SwapHomeComponent } from './swap/swap-home/swap-home.component';
 export { SwapResultComponent } from './swap/swap-result/swap-result.component';
+export { DashboardComponent } from './dashboard/dashboard.component';
