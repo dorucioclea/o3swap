f15b31d577f0d247e7f63aa0a84fb7b15c781bf2
bao <965001935@qq.com>
2021-04-07 23:29:00 +0800

    fix: dashboard

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -1,3 +1,4 @@
 export { SwapTokenComponent } from './compontent/swap-token/swap-token.component';
 export { SwapExchangeComponent } from './compontent/swap-exchange/swap-exchange.component';
 export { SwapSettingComponent } from './compontent/swap-setting/swap-setting.component';
+export { DashboardStakeComponent } from './compontent/dashboard-stake/dashboard-stake.component';
