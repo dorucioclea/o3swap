212cb7447378996fa4cd228f442167f002fc2a6d
haiwulei <1021391538@qq.com>
2021-04-07 15:50:28 +0800

    feat: uniswap

--- src/app/_lib/contract.ts
+++ src/app/_lib/contract.ts
@@ -13,6 +13,9 @@ export const SWAP_CONTRACT_HASH =
 export const ETH_SWAP_CONTRACT_HASH =
   NETWORK === 'MainNet' ? '' : '0x77F3A156e8E597C64d4a12d62f20a0d2ff839dD5';
 
+export const UNI_SWAP_CONTRACT_HASH =
+  NETWORK === 'MainNet' ? '' : '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
+
 export const SWAP_CONTRACT_CHAIN_ID = {
   BSC: NETWORK === 'MainNet' ? '' : 79,
   HECO: NETWORK === 'MainNet' ? '' : 7,
