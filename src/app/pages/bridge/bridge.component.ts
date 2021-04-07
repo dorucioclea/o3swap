29b17fb51176a5742d7c44134d0a8e9a9c564c06
bao <965001935@qq.com>
2021-04-07 19:51:50 +0800

    feat: bridge page

--- src/app/pages/bridge/bridge.component.ts
+++ src/app/pages/bridge/bridge.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ApiService, CommonService } from '@core';
+import {
+  SwapStateType,
+  SwapTransaction,
+  Token,
+  UPDATE_PENDING_TX,
+  ETH_TX_PAGES_PREFIX,
+  POLY_TX_PAGES_PREFIX,
+  NEO_TX_PAGES_PREFIX,
+  TxProgress,
+} from '@lib';
+import { Store } from '@ngrx/store';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { AnimationOptions } from 'ngx-lottie';
+import { interval, Observable, Unsubscribable } from 'rxjs';
 
+type PageStatus = 'home' | 'result';
+interface State {
+  swap: SwapStateType;
+}
 @Component({
   selector: 'app-bridge',
   templateUrl: './bridge.component.html',
-  styleUrls: ['./bridge.component.scss']
+  styleUrls: ['./bridge.component.scss'],
 })
-export class BridgeComponent implements OnInit {
+export class BridgeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    public store: Store<State>,
+    private apiService: ApiService,
+    private nzMessage: NzMessageService,
+    private commonService: CommonService
+  ) {
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+
   }
 
 }
