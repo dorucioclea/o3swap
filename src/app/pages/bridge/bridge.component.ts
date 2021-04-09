9562daa6d6447c6bbb5b32a4739fffdd712b0aa8
bao <965001935@qq.com>
2021-04-09 10:59:39 +0800

    fix: bridge

--- src/app/pages/bridge/bridge.component.ts
+++ src/app/pages/bridge/bridge.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, Output, OnDestroy, OnInit, EventEmitter } from '@angular/core';
 import { ApiService, CommonService } from '@core';
 import {
   SwapStateType,
@@ -12,9 +12,11 @@ import {
 } from '@lib';
 import { Store } from '@ngrx/store';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalService } from 'ng-zorro-antd/modal';
 import { AnimationOptions } from 'ngx-lottie';
 import { interval, Observable, Unsubscribable } from 'rxjs';
 import { SwapSettingComponent, SwapTokenComponent } from '@shared';
+import BigNumber from 'bignumber.js';
 
 type PageStatus = 'home' | 'result';
 interface State {
@@ -26,13 +28,28 @@ interface State {
   styleUrls: ['./bridge.component.scss'],
 })
 export class BridgeComponent implements OnInit, OnDestroy {
-  @Input() inputAmount: number = 0;
+  @Input() inputAmount: string;
+  @Input() fromToken: Token;
+  @Input() toToken: Token;
+
+  @Output() toInquiryPage = new EventEmitter<{
+    inputAmount: string;
+    fromToken: Token;
+    toToken: Token;
+  }>();
+  @Output() toResultPage = new EventEmitter();
+
+  changeData = false;
+  chooseSwapPath = {};
+  inputAmountFiat: string; // 支付的 token 美元价值
+  inputAmountError: string;
   rates = {};
 
 
   constructor(
     public store: Store<State>,
     private apiService: ApiService,
+    private modal: NzModalService,
     private nzMessage: NzMessageService,
     private commonService: CommonService
   ) {
@@ -44,6 +61,80 @@ export class BridgeComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
 
+  }
+
+  showTokens(type: 'from' | 'to'): void {
+    const modal = this.modal.create({
+      nzContent: SwapTokenComponent,
+      nzFooter: null,
+      nzTitle: null,
+      nzClosable: false,
+      nzClassName: 'custom-modal',
+      nzComponentParams: {
+        isFrom: type === 'from' ? true : false,
+        fromToken: this.fromToken,
+        toToken: this.toToken,
+      },
+    });
+    modal.afterClose.subscribe((res) => {
+      if (res) {
+        this.resetSwapData();
+        if (type === 'from') {
+          this.fromToken = res;
+          console.log(res)
+          this.checkInputAmountDecimal();
+          this.calcutionInputAmountFiat();
+        } else {
+          this.toToken = res;
+        }
+      }
+    });
+  }
+
+  checkInputAmountDecimal(): boolean {
+    const decimalPart = this.inputAmount && this.inputAmount.split('.')[1];
+    if (
+      this.fromToken &&
+      decimalPart &&
+      decimalPart.length > this.fromToken.decimals
+    ) {
+      this.inputAmountError = `You've exceeded the decimal limit.`;
+      return false;
+    }
+    // neo nneo 互换只能整单位
+    if (
+      this.fromToken &&
+      this.fromToken.symbol === 'nNEO' &&
+      this.toToken &&
+      this.toToken.symbol === 'NEO' &&
+      decimalPart &&
+      decimalPart.length > 0
+    ) {
+      this.inputAmountError = `You've exceeded the decimal limit.`;
+      return false;
+    }
+    this.inputAmountError = '';
+    return true;
+  }
+
+  resetSwapData(): void {
+    this.changeData = true;
+    this.chooseSwapPath = {};
+  }
+
+  calcutionInputAmountFiat(): void {
+    if (!this.fromToken) {
+      return;
+    }
+    const price = this.rates[this.fromToken.rateName];
+    if (this.inputAmount && price) {
+      this.inputAmountFiat = new BigNumber(this.inputAmount)
+        .multipliedBy(new BigNumber(price))
+        .dp(2)
+        .toFixed();
+    } else {
+      this.inputAmountFiat = '';
+    }
   }
 
   changeInputAmount($event): void {
