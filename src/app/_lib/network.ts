154795831e06fc38b6f35cc8e92a36dbf9fb4cba
hkhv <746392783@qq.com>
2021-04-18 19:11:13 +0800

    chore: add network config

--- src/app/_lib/network.ts
+++ src/app/_lib/network.ts
@@ -1,3 +1,5 @@
+import { environment } from '@env/environment';
+
 export type Network = 'MainNet' | 'TestNet';
 
-export const NETWORK: Network = 'MainNet';
+export const NETWORK: Network = environment.network as Network;
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiDomain: '',
+  network: 'MainNet',
 };
