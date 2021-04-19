5c2e8403f1acf86054d70c8e56b1051e220a2ac7
hkhv <746392783@qq.com>
2021-04-19 21:03:31 +0800

    fix: update wallet connect

--- src/app/shared/compontent/header/wallet-connect/wallet-connect.component.ts
+++ src/app/shared/compontent/header/wallet-connect/wallet-connect.component.ts
@@ -121,6 +121,9 @@ export class WalletConnectComponent implements OnInit, OnDestroy {
 
   async connectEthWallet(wallet: EthWallet): Promise<void> {
     let connectRes;
+    if (wallet.name === 'O3') {
+      return;
+    }
     switch (this.connectChainType) {
       case 'ETH':
         if (this.ethWalletName === wallet.name) {
@@ -146,11 +149,11 @@ export class WalletConnectComponent implements OnInit, OnDestroy {
           this.connectChainType
         );
         break;
-      case 'O3':
-        connectRes = await this.o3EthWalletApiService.connect(
-          this.connectChainType
-        );
-        break;
+      // case 'O3':
+      //   connectRes = await this.o3EthWalletApiService.connect(
+      //     this.connectChainType
+      //   );
+      //   break;
     }
     if (this.connectOne && connectRes) {
       this.close();
